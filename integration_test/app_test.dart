42b8582ec68cc8539d6c99ef59b49c3e7bca6368
HenriqueHartmann <alemaohartmann@gmail.com>
2024-02-19 01:20:12 -0300

    Add test to the switch of change all cards background color

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:tcc_performance_app/main.dart' as app;
+import 'package:tcc_performance_app/src/features/setState/main_setState.dart';
 
 void main() {
   // Ensure that the emulator is initialized
@@ -30,8 +31,49 @@ void main() {
       // Define the key need to check weather selection was successful
       const setStatePageKey = const Key('setStatePage');
 
-      // Expecter
+      // Expecters
       expect(find.byKey(setStatePageKey), findsOneWidget);
+    });
+
+    testWidgets('Change all card colors in SetState page',
+        (WidgetTester tester) async {
+      // Initiate an instance of the App
+      app.main();
+
+      // Wait for the content to be loaded
+      await tester.pumpAndSettle();
+
+      // Define the keys needed for the selection test
+      const setStateMainKey = const Key('setStateSelection');
+      const setStateChildKey = const Key('libSelectionItem');
+
+      // Choose the setState option by tapping the the libSelectionItem Key from the parent setStateSelection Key
+      await tester.tap(find.ancestor(
+          of: find.byKey(setStateChildKey),
+          matching: find.byKey(setStateMainKey)));
+
+      // Wait once again for the content to be loaded
+      await tester.pumpAndSettle();
+
+      // Define the key need to check weather selection was successful
+      const setStatePageKey = const Key('setStatePage');
+
+      expect(find.byKey(setStatePageKey), findsOneWidget);
+
+      // Define the key need to check weather selection was successful
+      const changeAllColorsSwitchKey = const Key('changeAllCardColors');
+      const switchWidgetKey = const Key('switchWidget');
+
+      await tester.tap(find.descendant(
+          of: find.byKey(changeAllColorsSwitchKey),
+          matching: find.byKey(switchWidgetKey)));
+
+      // Wait once again for the content to be loaded
+      await tester.pumpAndSettle();
+
+      final MainSetStateState state = tester.state(find.byType(MainSetState));
+
+      expect(state.allCardsBackgroundColorSwitch, true);
     });
   });
 
